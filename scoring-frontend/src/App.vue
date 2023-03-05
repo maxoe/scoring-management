ad2b198768f07cebc154ad4899bc29b2d9a3f2f0
Max Oesterle <max_oesterle@web.de>
2023-03-05 15:00:30 +0100

    Add frontend

---
+++ scoring-frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<div id="app">
+		<HelloWorld msg="Welcome to the scoring management" />
+	</div>
+</template>
---
+++ scoring-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>{{ msg }}</h1>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
