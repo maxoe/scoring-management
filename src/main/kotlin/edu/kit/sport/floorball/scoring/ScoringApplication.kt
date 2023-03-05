885ec4d6d79b4665d06f60a0e5abe2850b255272
Max Oesterle <max_oesterle@web.de>
2023-03-05 12:26:52 +0100

    Springboot init

---
+++ src/main/kotlin/edu/kit/sport/floorball/scoring/ScoringApplication.kt
@@ -0,0 +1,11 @@
+package edu.kit.sport.floorball.scoring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScoringApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScoringApplication>(*args)
+}
---
+++ src/test/kotlin/edu/kit/sport/floorball/scoring/ScoringApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.kit.sport.floorball.scoring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
